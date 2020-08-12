91c3f0576552bb66b709fb2e605f64d9a75daef4
vinitsiriya <vinitsiriya@gmail.com>
2020-08-12 10:19:40 -0700

    added files

---
+++ ExampleClient/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Buffers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExampleClient
+{
+    public class Program
+    {
+
+        public static async Task Main(String[] args)
+        {
+            EndPoint iPEnd = new IPEndPoint(IPAddress.Any, 8084);
+            var serverSocket = new Socket(iPEnd.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.NoDelay = true;
+            serverSocket.Bind(iPEnd);
+            serverSocket.Listen(0);
+
+            var acceptTask = serverSocket.AcceptAsync();
+
+
+
+
+            EndPoint endPointClient = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8084);
+
+            var clientSocket = new Socket(endPointClient.AddressFamily, SocketType.Stream, ProtocolType.Tcp)
+            {
+                NoDelay = true
+            };
+            var clientThread = clientSocket.ConnectAsync(endPointClient);
+            clientThread.Wait();
+            byte[] databytes = UTF8Encoding.UTF8.GetBytes("Hello hello ghellpo helloooeoeoeoeoeoeoeoe ");
+         //   clientSocket.Send(databytes);
+            {
+             
+                var memorypool = System.Buffers.SlabMemoryPoolFactory.Create();
+                PipeSocket.PipeSocket clientPipeSocket = new PipeSocket.PipeSocket(clientSocket, memorypool, PipeScheduler.Inline);
+                clientPipeSocket.Start();
+
+               // byte[] databytes = UTF8Encoding.UTF8.GetBytes("Hello hello ghellpo helloooeoeoeoeoeoeoeoe ");
+                ReadOnlyMemory<byte> data = new ReadOnlyMemory<byte>(databytes);
+                for (int i = 0; i < 100; i++)
+                {
+                    await clientPipeSocket.Transport.Output.WriteAsync(data, CancellationToken.None);
+                    await clientPipeSocket.Transport.Output.FlushAsync();
+                }
+             
+                
+            }
+            acceptTask.Wait();
+
+
+            var serverSoc = acceptTask.Result;
+          
+            {
+                var scheduler = PipeScheduler.Inline;
+                var memorypool = System.Buffers.SlabMemoryPoolFactory.Create();
+                PipeSocket.PipeSocket pipeSocket = new PipeSocket.PipeSocket(serverSoc, memorypool, PipeScheduler.Inline);
+                pipeSocket.Start();
+
+
+                PipeReader pipeReader = pipeSocket.Transport.Input;
+                while (pipeReader.TryRead(out var readResult))
+                {
+                    var data = readResult.Buffer;
+                    var str = UTF8Encoding.UTF8.GetString(data);
+                    pipeReader.AdvanceTo(data.End, data.End);
+                    Console.WriteLine(str);
+                }
+                
+
+          
+            }
+
+           
+
+
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MemoryPool/MemoryPoolFactory.cs
@@ -0,0 +1,24 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Buffers;
+
+namespace System.Buffers
+{
+    public static class SlabMemoryPoolFactory
+    {
+        public static MemoryPool<byte> Create()
+        {
+#if DEBUG
+            return new DiagnosticMemoryPool(CreateSlabMemoryPool());
+#else
+            return CreateSlabMemoryPool();
+#endif
+        }
+
+        public static MemoryPool<byte> CreateSlabMemoryPool()
+        {
+            return new SlabMemoryPool();
+        }
+    }
+}
